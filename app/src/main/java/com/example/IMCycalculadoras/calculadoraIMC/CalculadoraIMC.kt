109d646f227b31ac7fc7da650814dbec98d38b31
eloymartos <115500079+eloymartos@users.noreply.github.com>
2023-10-28 17:27:16 +0200

    xml terminado

--- app/src/main/java/com/example/IMCycalculadoras/calculadoraIMC/CalculadoraIMC.kt
+++ app/src/main/java/com/example/IMCycalculadoras/calculadoraIMC/CalculadoraIMC.kt
@@ -3,19 +3,40 @@ package com.example.IMCycalculadoras.calculadoraIMC
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.example.IMCycalculadoras.R
 import com.google.android.material.slider.RangeSlider
 
 class CalculadoraIMC : AppCompatActivity() {
+
+    lateinit var botonesGenero : Array<CardView>
+
+    lateinit var medidorAltura : RangeSlider
+
+    lateinit var altura : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculadora_imc)
+        init()
+    }
 
-        val texto = findViewById<TextView>(R.id.cms)
-        val rangeSlider = findViewById<RangeSlider>(R.id.rangoAltura)
-        rangeSlider.addOnChangeListener { _, value, _ ->
-            texto.text = value.toInt().toString()
+    private fun init(){
+
+        altura = findViewById<TextView>(R.id.cms)
+
+        medidorAltura = findViewById<RangeSlider>(R.id.heigh_range)
+
+        initRangeSlider()
+
+        botonesGenero = arrayOf(findViewById(R.id.cardView_men), findViewById(R.id.cardaView_women))
+
+    }
+
+    private fun initRangeSlider(){
+
+        medidorAltura.addOnChangeListener { _, value, _ ->
+            altura.text = value.toInt().toString()
         }
-        rangeSlider.stepSize = 1f
+        medidorAltura.stepSize = 1f
     }
 }
