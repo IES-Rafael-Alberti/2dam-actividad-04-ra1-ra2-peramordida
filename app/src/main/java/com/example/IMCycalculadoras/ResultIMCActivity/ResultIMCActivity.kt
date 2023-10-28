3ecef3ed46f59db16e5b904cb76c0bf8c11354ff
eloymartos <115500079+eloymartos@users.noreply.github.com>
2023-10-28 17:29:08 +0200

    xml terminado

---
+++ app/src/main/java/com/example/IMCycalculadoras/ResultIMCActivity/ResultIMCActivity.kt
@@ -0,0 +1,12 @@
+package com.example.IMCycalculadoras.ResultIMCActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.IMCycalculadoras.R
+
+class ResultIMCActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_imcactivity)
+    }
+}
