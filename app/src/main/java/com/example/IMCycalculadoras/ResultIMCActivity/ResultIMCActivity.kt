7c5bcf781db40f669a9b08b532d774898d6669ce
eloymartos <115500079+eloymartos@users.noreply.github.com>
2023-10-29 17:34:18 +0100

    implementar imagen segun resultado

--- app/src/main/java/com/example/IMCycalculadoras/ResultIMCActivity/ResultIMCActivity.kt
+++ app/src/main/java/com/example/IMCycalculadoras/ResultIMCActivity/ResultIMCActivity.kt
@@ -1,6 +1,8 @@
 package com.example.IMCycalculadoras.ResultIMCActivity
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.IMCycalculadoras.R
@@ -11,6 +13,12 @@ class ResultIMCActivity : AppCompatActivity() {
     val df = DecimalFormat("#.##")
 
     lateinit var resultado:String
+
+    lateinit var answerText : TextView
+
+    lateinit var answerNumber : TextView
+
+    lateinit var answerImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,32 +33,38 @@ class ResultIMCActivity : AppCompatActivity() {
 
     private fun textFiller(){
 
-        val answerNumber = findViewById<TextView>(R.id.answer_number)
-            answerNumber.text=resultado.toString()
-        val answerText = findViewById<TextView>(R.id.answer_phrase)
+        answerNumber = findViewById<TextView>(R.id.answer_number)
+        answerNumber.text=resultado.toString()
+        answerText = findViewById<TextView>(R.id.answer_phrase)
+        answerImage = findViewById(R.id.weight_img)
+
 
         answerText.text = when(resultado.toDouble()){
             in 0f..18.5f->{
                 answerNumber.setTextColor(resources.getColor(R.color.orange))
                 answerText.setTextColor(resources.getColor(R.color.orange))
+                answerImage.setImageResource(R.drawable.underweight)
                 "Peso inferior al normal"
             }
             in 18.5f..24.9f->{
 
                 answerNumber.setTextColor(resources.getColor(R.color.white))
                 answerText.setTextColor(resources.getColor(R.color.white))
+                answerImage.setImageResource(R.drawable.standard)
                 "Peso normal"
             }
             in 25f..29.9f->{
 
                 answerNumber.setTextColor(resources.getColor(R.color.slight_dark_blue))
                 answerText.setTextColor(resources.getColor(R.color.slight_dark_blue))
+                answerImage.setImageResource(R.drawable.overweight)
                 "Peso superior al normal"
             }
             else->{
 
                 answerNumber.setTextColor(resources.getColor(R.color.orange))
                 answerText.setTextColor(resources.getColor(R.color.orange))
+                answerImage.setImageResource(R.drawable.obese)
                 "Obesidad"
             }
         }
